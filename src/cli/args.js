8a1c8c17a380cb46fc0d228e8f0215fbe76d2094
NeroiN <NeroiN2@yandex.ru>
2024-02-21 08:32:07 +0300

    relise args.js file

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+     // Получаем аргументы командной строки, начиная с третьего элемента (первые два - путь к Node.js и скрипту)
+  const args = process.argv.slice(2);
+
+  // Проходим по аргументам
+  for (let i = 0; i < args.length; i += 2) {
+    // Получаем название свойства и значение
+    const propName = args[i].slice(2); // убираем "--"
+    const propValue = args[i + 1];
+
+    // Выводим на консоль в требуемом формате
+    console.log(`${propName} is ${propValue}`);
+  }
 };
 
 parseArgs();
