a567ab13b7f18bf8285670afc364504f0b5ce210
NeroiN <NeroiN2@yandex.ru>
2024-02-21 08:46:55 +0300

    Revert "relise args.js file"
    This reverts commit 8a1c8c17a380cb46fc0d228e8f0215fbe76d2094.

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,16 +1,5 @@
 const parseArgs = () => {
-     // Получаем аргументы командной строки, начиная с третьего элемента (первые два - путь к Node.js и скрипту)
-  const args = process.argv.slice(2);
-
-  // Проходим по аргументам
-  for (let i = 0; i < args.length; i += 2) {
-    // Получаем название свойства и значение
-    const propName = args[i].slice(2); // убираем "--"
-    const propValue = args[i + 1];
-
-    // Выводим на консоль в требуемом формате
-    console.log(`${propName} is ${propValue}`);
-  }
+    // Write your code here 
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,17 +1,5 @@
 const parseEnv = () => {
-    
-    const prefix = 'MITSO_';
-    const envVariables = process.env;
-    const mitsoVariables = Object.keys(envVariables)
-        .filter(key => key.startsWith(prefix))
-        .map(key => `${key}=${envVariables[key]}`)
-        .join('; ');
-    if (mitsoVariables) {
-        console.log(mitsoVariables);
-    } else {
-        console.error('No MITSO variables found in the environment.');
-    }
-    
+    // Write your code here 
 };
 
 parseEnv();
