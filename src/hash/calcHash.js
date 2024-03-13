75fda4631113cf7591ba7c2fd3312095e309512e
NeroiN <NeroiN2@yandex.ru>
2024-03-13 08:48:51 +0300

    relise args.js and env.js file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,51 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises';
+import path from 'path';
 
-await calculateHash();
+async function calcHash() {
+    const sourceFolder = '/src/hash/files';
+    const oldFilename = 'fileToCalculateHashFor.txt';
+
+    const sourceFilePath = path.join(sourceFolder, oldFilename);
+
+    try {
+        await fs.access(sourceFilePath);
+        console.error('Not file in folder');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            try {
+                const bufferText = Buffer.from(oldFilename, 'utf8');
+                console.log(bufferText.toString('hex'));
+            } catch {
+                
+            }
+        }
+    }
+
+}
+
+calcHash();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const bufferText = Buffer.from('hello world', 'utf8'); // or Buffer.from('hello world')
+// console.log(bufferText); // <Buffer 68 65 6c 6c 6f 20 77 6f 72 6c 64>
+
+// const text = bufferText.toString('hex');
+// // To get hex
+// console.log(text); // 68656c6c6f20776f726c64
+
+// console.log(bufferText.toString()); // or toString('utf8')
+// // hello world
+
+// //one single line
+// // Buffer.from('hello world').toString('hex')
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,30 @@
+import fs from 'fs/promises';
+import path from 'path';
 const read = async () => {
-    // Write your code here 
+    
+    // Имя файла для чтения и путь к файлу
+    const fileName = 'fileToRead.txt';
+    const filePath = path.join(__dirname, 'src/streams/files', fileName);
+    
+    // Создаем Readable Stream
+    const readableStream = fs.readableStream(filePath, { encoding: 'utf8' });
+    
+    // Устанавливаем обработчик события 'data' для чтения данных из потока
+    readableStream.on('data', (chunk) => {
+      // Печатаем содержимое в process.stdout
+      process.stdout.write(chunk);
+    });
+    
+    // Устанавливаем обработчик события 'end' для завершения потока
+    readableStream.on('end', () => {
+      console.log('\nЧтение завершено');
+    });
+    
+    // Устанавливаем обработчик события 'error' для обработки ошибок
+    readableStream.on('error', (err) => {
+      console.error(`Ошибка при чтении файла: ${err.message}`);
+    });
+    
 };
 
 await read();
